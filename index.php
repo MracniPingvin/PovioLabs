d2d7a0938f2b307a53ce93ee17c08713a479f5bb
MracniPingvin <miha.cerne1@gmail.com>
2016-06-25 18:19:02 +0200

    Added first segment of index page

--- index.php
+++ index.php
@@ -9,9 +9,27 @@
 	<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/body.css">
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 </head>
 <body>
-	<?php include 'includes/header.html';?>
+	<div class="container-fluid">
+  		<div id="header" class="row">
+			<?php include 'includes/header.html';?>
+		</div>
+		<div class="row">
+			<div class="base" id="welcome">
+				<div id="welcome_div">
+
+					<p><strong>Notable Labs is a service</strong> to help your oncologist identify<br>promising treatment options <strong>for you.</strong></p>
+					<img src="images/arrow-down.png" id="arrow_img">
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+
+
 
 
 	
