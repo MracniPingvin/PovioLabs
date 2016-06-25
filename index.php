12806b1a07a593ea1e256628609c6e9b4ea2bc8f
MracniPingvin <miha.cerne1@gmail.com>
2016-06-25 16:19:36 +0200

    Initial commit, added header

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Example Domain</title>
+
+    <meta charset="utf-8" />
+   	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/body.css">
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<?php include 'includes/header.html';?>
+
+
+	
+</body>    
+</html>
